197f01cda4be30d5684a9dbc4fb71afac5d96a97
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2022-07-13 21:13:52 -0300

    Inserção do meu primeiro programa em Java, seguindo as orientações da trilha do bootcamp Code Girls

---
+++ primprog-dio/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+import br.com.dio.model.Numero;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println("Exibir classe Gato: \n" + gato);
+        Numero numero = new Numero();
+        System.out.println("\nExibir classe Numero: \n" + numero);
+    }
+}
+
